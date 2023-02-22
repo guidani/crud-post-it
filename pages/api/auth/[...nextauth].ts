f085faa828890e769d870872044d1e3b0bf9025f
guilherme daniel <gui.dani13@gmail.com>
2023-02-22 16:58:57 -0300

    adicionado autenticação do google com integração entre next auth e prisma

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,24 @@
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@next-auth/prisma-adapter"
+import client from "../../../prisma/client";
+
+
+export const authOptions = {
+  // Configure one or more authentication providers
+  adapter: PrismaAdapter(client),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID || "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+    }),
+    GithubProvider({
+      clientId: process.env.GITHUB_ID || "",
+      clientSecret: process.env.GITHUB_SECRET || "",
+    }),
+    // ...add more providers here
+  ],
+};
+
+export default NextAuth(authOptions);
